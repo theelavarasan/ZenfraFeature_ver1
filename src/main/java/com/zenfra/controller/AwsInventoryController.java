09da06b2c155c9a4e1da9bb5172b1a60ed0c9896
javateam <javateam@virtualtechgurus.com>
2021-05-24 14:52:50 +0530

    Commit for aws

--- src/main/java/com/zenfra/controller/AwsInventoryController.java
+++ src/main/java/com/zenfra/controller/AwsInventoryController.java
@@ -9,25 +9,36 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.hadoop.yarn.webapp.hamlet.Hamlet.A;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonObject;
 import com.zenfra.configuration.AwsInventoryPostgresConnection;
 import com.zenfra.model.AwsInventory;
 import com.zenfra.model.ResponseModel_v2;
 import com.zenfra.utils.CommonFunctions;
 import com.zenfra.utils.DBUtils;
+
+import io.netty.handler.codec.http.HttpRequest;
 
 @RestController
 @RequestMapping("/rest/aws-inventory")
@@ -51,12 +62,13 @@ public class AwsInventoryController {
 			String connection=checkConnection(aws.getAccess_key_id(), aws.getSecret_access_key());
 				System.out.println(connection);
 				
-			if(connection.contains("fail")&& connection.contains("InvalidClientTokenId")) {
+			if(connection.contains("exception") || connection.isEmpty() || (connection.contains("fail")&& connection.contains("InvalidClientTokenId"))) {
 				responseModel.setResponseDescription("InvalidClientTokenId");
-				responseModel.setResponseCode(HttpStatus.BAD_REQUEST);
-				responseModel.setjData(map.readValue(connection, JSONObject.class));
+				responseModel.setResponseCode(HttpStatus.BAD_REQUEST);				
+				if(connection.contains("fail")) {
+					responseModel.setjData(map.readValue(connection, JSONObject.class));
+				}
 				return responseModel;
-				
 			}
 				
 			aws.setSecret_access_key(sha256hex);
@@ -96,7 +108,7 @@ public class AwsInventoryController {
 		try {
 			
 			String query="select * from aws_cloud_credentials where sitekey=':sitekey_value'";
-			query=query.replace(":sitekey_value", sitekey);
+			query=query.replace(":sitekey_value", sitekey);//.replace(":access_key_id", accountid);
 			System.out.println(query);
 		   Connection conn =post.getPostConnection();
 		   Statement stmt = conn.createStatement();			
@@ -133,7 +145,7 @@ public class AwsInventoryController {
 	public ResponseModel_v2 getLastFourValuesBySiteKey(@PathVariable String sitekey) {
 		
 		ResponseModel_v2 model=new ResponseModel_v2();
-		try {
+	try {
 		String query="select * from aws_cloud_credentials where sitekey=':sitekey_value'";
 		query=query.replace(":sitekey_value", sitekey);
 		System.out.println(query);
@@ -166,6 +178,76 @@ public class AwsInventoryController {
 
 	
 	
+	@GetMapping("/call-script")
+	public ResponseModel_v2 callScript(
+			@RequestParam String siteKey,
+			@RequestParam String userId,@RequestParam String tenantId,
+			HttpServletRequest request
+			) {
+		
+		//System.out.println("test");
+		
+		ResponseModel_v2 model=new ResponseModel_v2();
+		try {
+			String token=request.getHeader("Authorization").replace("Bearer ", "");
+			token="Bearer "+token;
+			System.out.println(token);
+			
+			Object insert=insertLogUploadTable(siteKey, tenantId, userId, token);
+			
+			ObjectMapper map=new ObjectMapper();
+			
+			JSONObject resJson=map.convertValue(insert, JSONObject.class);
+			JSONObject body=map.readValue(resJson.get("body").toString(), JSONObject.class);
+			
+			
+			System.out.println(body);
+			if(body!=null && body.containsKey("status")&& !body.get("status").equals("200")) {
+				model.setjData(resJson);
+				model.setResponseCode(HttpStatus.INTERNAL_SERVER_ERROR);
+				return model;
+			}
+			
+			Object res=callAwsScript(siteKey,userId,token) ; 
+			
+		
+			model.setResponseCode(HttpStatus.OK);
+			model.setjData(res);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			model.setResponseCode(HttpStatus.INTERNAL_SERVER_ERROR);
+			model.setResponseDescription(e.getMessage());
+		}
+		
+		return model;
+	}
+	
+	
+	private Object callAwsScript(String siteKey, String userId, String token) {
+		
+		String responce="";
+		try {
+			
+			String path=" /opt/ZENfra/repo/cloud-inventory-collectors/aws/inventory_collection/aws_inventory.py";
+			String cmd="python3 "+path+" --sitekey "+siteKey;
+		
+			Process process = Runtime.getRuntime().exec(cmd);
+			 BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			    String line = "";			    
+			    while ((line = reader.readLine()) != null) {
+			    	responce+=line;
+			    }
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return responce;
+	}
+
+
 	public String checkConnection(String access_id,String secret_key) {
 		
 		//Map<String, String> map=DBUtils.getPostgres();
@@ -173,7 +255,7 @@ public class AwsInventoryController {
 		try {
 			String path="/opt/ZENfra/repo/cloud-inventory-collectors/aws/authentication.py";
 			String cmd="python3 "+path+" --id "+access_id+" --key "+secret_key;
-			System.out.println(cmd);
+			
 			Process process = Runtime.getRuntime().exec(cmd);
 			 BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
 			    String line = "";			    
@@ -183,7 +265,44 @@ public class AwsInventoryController {
 			
 		} catch (Exception e) {
 			e.printStackTrace();
+			return "exception";
 		}
 		return responce;
 	}
+	
+	
+	
+	
+	public Object insertLogUploadTable(String siteKey,
+		String tenantId,String userId,String token) {
+		  Object responce=null;
+		try {
+			JSONObject obj=new JSONObject();
+				obj.put("logType", "AWS");
+				obj.put("description", "AWS data retrieval");
+				obj.put("siteKey", siteKey);
+				obj.put("userId", userId);
+				obj.put("tenantId", tenantId);
+				obj.put("uploadAndProcess", false);
+			
+		 RestTemplate restTemplate=new RestTemplate();
+		 HttpEntity<Object> request = new HttpEntity<>(obj.toJSONString(),createHeaders(token));
+          responce= restTemplate
+                 .exchange("http://uat.zenfra.co:8080/parsing/upload", HttpMethod.POST, request, String.class);	
+			
+         System.out.println(responce);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return responce;
+	}
+	
+	
+	 HttpHeaders createHeaders(String token){
+	        return new HttpHeaders() {{
+	              set( "Authorization", token );
+	            setContentType(MediaType.APPLICATION_JSON);
+	        }};
+	    }
 }
