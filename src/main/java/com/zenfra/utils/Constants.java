29a43f1caa1830150fac799c3cb8a81d96a22049
javateam <javateam@virtualtechgurus.com>
2021-06-02 18:39:34 +0530

    change host

--- src/main/java/com/zenfra/utils/Constants.java
+++ src/main/java/com/zenfra/utils/Constants.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Component;
 @Component
 public class Constants {
 	
-	public static String current_url="http://uat.zenfra.co:8080";
+	public static String current_url="http://localhost:8080";
 }
