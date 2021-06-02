5a89d2a66fda35b6e58aca5af010aa27de1259bb
javateam <javateam@virtualtechgurus.com>
2021-06-02 17:05:56 +0530

    commit for aws

--- src/main/java/com/zenfra/controller/AwsInventoryController.java
+++ src/main/java/com/zenfra/controller/AwsInventoryController.java
@@ -181,8 +181,8 @@ public class AwsInventoryController {
 		
 		ResponseModel_v2 model=new ResponseModel_v2();
 		try {
-			String token=request.getHeader("Authorization");
-			//String token="Bearer "+common.getZenfraToken("aravind.krishnasamy@virtualtechgurus.com", "Aravind@123");
+			//String token=request.getHeader("Authorization");
+			String token="Bearer "+common.getZenfraToken("aravind.krishnasamy@virtualtechgurus.com", "Aravind@123");
 			System.out.println(token);
 			
 		
