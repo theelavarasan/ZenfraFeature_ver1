14123ed5d6976a50f82089ee18a7e0f548c88ba3
javateam <javateam@virtualtechgurus.com>
2021-06-08 23:51:49 +0530

    Default IST

--- src/main/java/com/zenfra/ftp/scheduler/ScheduleTaskService.java
+++ src/main/java/com/zenfra/ftp/scheduler/ScheduleTaskService.java
@@ -45,7 +45,7 @@ public class ScheduleTaskService {
 		
 		// Schedule Task to be executed every night at 00 or 12 am
 		public void addTaskToScheduler(long id,Runnable task,String corn,String timeZone) {//"0 0 0 * * ?"
-			ScheduledFuture<?> scheduledTask = scheduler.schedule(task, new CronTrigger(corn, TimeZone.getTimeZone(timeZone)));
+			ScheduledFuture<?> scheduledTask = scheduler.schedule(task, new CronTrigger(corn, TimeZone.getTimeZone(TimeZone.getDefault().getID())));//TimeZone.getTimeZone(TimeZone.getDefault().getID())
 			jobsMap.put(id, scheduledTask);
 		}
 		
