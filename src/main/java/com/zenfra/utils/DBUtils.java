bb4f1a7c1ec85e865c0e43db2019deffac45636e
javateam <javateam@virtualtechgurus.com>
2021-06-14 13:53:18 +0530

    Add present server URL.

--- src/main/java/com/zenfra/utils/DBUtils.java
+++ src/main/java/com/zenfra/utils/DBUtils.java
@@ -100,4 +100,20 @@ public class DBUtils {
 		}
 		return map;
 	}
+	
+	public static String getServerUrl(){
+			String url="localhost:8080";
+		try {
+			ZookeeperConnection zkConnection = new ZookeeperConnection();
+			ZKModel.zkData = zkConnection.getZKData();
+			url=ZKModel.getProperty(ZKConstants.APP_SERVER_PROTOCOL.toString());
+			url+="://"+ZKModel.getProperty(ZKConstants.APP_SERVER_IP);
+			url+=":"+ZKModel.getProperty(ZKConstants.APP_SERVER_PORT);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return url;
+	}
+	
+	
 }
