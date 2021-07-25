006749ff08457f97763b6829c6827fa0ab78f3da
Senthil <Senthil@DESKTOP-MFOO58G>
2021-07-25 23:28:15 +0530

    logs added

--- src/main/java/com/zenfra/service/HealthCheckService.java
+++ src/main/java/com/zenfra/service/HealthCheckService.java
@@ -131,6 +131,7 @@ public class HealthCheckService {
 		JSONArray resultArray = new JSONArray();
 		try {
 			List<Object> resultList = healthCheckDao.getEntityListByColumn("select health_check_name from health_check where site_key='"+siteKey+"'", HealthCheck.class);
+			System.out.println("------------resultList--------------" + resultList.size());
 			if(resultList != null && !resultList.isEmpty()) {
 				for(Object obj : resultList) {
 					if(obj instanceof String) {						
@@ -141,7 +142,7 @@ public class HealthCheckService {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+		System.out.println("------------resultArray--------------" + resultArray);
 		return resultArray;
 	}
 	
