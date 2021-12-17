0977d227d0b5d12a108dfba4da6bdaf02e7b2909
Ahmed <ahmed.mohammed@virtualtechgurus.com>
2021-12-17 16:55:11 +0530

    Cors error fix

--- src/main/java/com/zenfra/controller/EolAndEosSoftwareController.java
+++ src/main/java/com/zenfra/controller/EolAndEosSoftwareController.java
@@ -2,7 +2,7 @@ package com.zenfra.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.zenfra.model.EolAndEosSoftwareModel;
 import com.zenfra.service.EolAndEosSoftwareService;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/rest/eol-eos-sw")
 public class EolAndEosSoftwareController {
