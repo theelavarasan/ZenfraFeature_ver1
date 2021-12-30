9e2d038d99cde170c9574507cce599d4a3bc14d6
Senthil <Senthil@DESKTOP-MFOO58G>
2021-12-30 20:32:16 +0530

    fix mass update issue

--- src/main/java/com/zenfra/dao/EolAndEosHardwareRepository.java
+++ src/main/java/com/zenfra/dao/EolAndEosHardwareRepository.java
@@ -1,6 +1,8 @@
 package com.zenfra.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.zenfra.model.EolAndEosHardwareIdentityModel;
@@ -8,5 +10,8 @@ import com.zenfra.model.EolAndEosHardwareModel;
 
 @Repository
 public interface EolAndEosHardwareRepository extends JpaRepository<EolAndEosHardwareModel, EolAndEosHardwareIdentityModel>{
+
+	@Query("select e from EolAndEosHardwareModel e where e.eol_eos_hw_id=:eol_eos_hw_id")
+	EolAndEosHardwareModel findByHwId(@Param("eol_eos_hw_id") String eol_eos_hw_id);
 
 }
--- src/main/java/com/zenfra/dao/EolAndEosSoftwareRepository.java
+++ src/main/java/com/zenfra/dao/EolAndEosSoftwareRepository.java
@@ -1,6 +1,8 @@
 package com.zenfra.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.zenfra.model.EolAndEosSoftwareIdentityModel;
@@ -8,5 +10,10 @@ import com.zenfra.model.EolAndEosSoftwareModel;
 
 @Repository
 public interface EolAndEosSoftwareRepository extends JpaRepository<EolAndEosSoftwareModel, EolAndEosSoftwareIdentityModel>{
+
+	@Query("select e from EolAndEosSoftwareModel e where e.eol_eos_sw_id=:eol_eos_sw_id")
+	EolAndEosSoftwareModel findBySwId(@Param("eol_eos_sw_id") String eol_eos_sw_id);
+
+	
 
 }
