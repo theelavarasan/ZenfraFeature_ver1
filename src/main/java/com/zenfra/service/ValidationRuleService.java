f9b6088788b3f834c366e691565d70eed25f51ac
pravin <pravinkumar.krishnasamy@virtualtechgurus.com>
2022-01-17 19:30:13 +0530

    debugging the migration method validation rule

--- src/main/java/com/zenfra/service/ValidationRuleService.java
+++ src/main/java/com/zenfra/service/ValidationRuleService.java
@@ -402,7 +402,7 @@ public class ValidationRuleService {
 			//JSONParser parser = new JSONParser();
 			System.out.println("!!!!! valueArray: " + valueArray);
 			for(Map<String, Object> list : valueArray) {
-				resultArray = (JSONArray) parser.parse(list.get("columnValues").toString());
+				resultArray = (JSONArray) parser.parse(list.get("data_value").toString());
 			}
 		} catch(Exception e) {
 			e.printStackTrace();
