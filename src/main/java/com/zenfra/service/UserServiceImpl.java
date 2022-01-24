7759a96889e2d1afec7bf2c01b60ddcb5656212b
pravin <pravinkumar.krishnasamy@virtualtechgurus.com>
2022-01-24 20:41:27 +0530

    upddated Log4j version for vulnerability issue

--- src/main/java/com/zenfra/service/UserServiceImpl.java
+++ src/main/java/com/zenfra/service/UserServiceImpl.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Service;
 import com.zenfra.utils.ZenfraFeaturesRestApis;
 
 
-@Service(value = "userService")
+@Service
 public class UserServiceImpl implements UserDetailsService {
 
 	@Autowired
