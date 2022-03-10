603b58f2a876cfc069e6b4fc4d5c257074982e1d
mohanaprakash <mohanaprakash.jayabalan@virtualtechgurus.com>
2022-03-10 19:46:46 +0530

    updated and created by checked null for validation rule

--- src/main/java/com/zenfra/service/HealthCheckService.java
+++ src/main/java/com/zenfra/service/HealthCheckService.java
@@ -282,9 +282,9 @@ public class HealthCheckService {
 		}
 		
 
-		if (healthCheck.getCreateBy() != null && healthCheck.getCreateBy().equalsIgnoreCase(healthCheck.getUpdateBy())) {
-			response.put("updatedBy", user.getFirst_name() + " " + user.getLast_name());
-		} else if (healthCheck.getCreateBy() != null && !healthCheck.getCreateBy().equalsIgnoreCase(healthCheck.getUpdateBy())) {
+		if (healthCheck.getCreateBy().equalsIgnoreCase(healthCheck.getUpdateBy())) {
+			response.put("updatedBy", user.getFirst_name() != null ? user.getFirst_name() : "" + " " + user.getLast_name() != null ? user.getLast_name() : "");
+		} else if (!healthCheck.getCreateBy().equalsIgnoreCase(healthCheck.getUpdateBy())) {
 			Users updateUser = userCreateService.getUserByUserId(healthCheck.getUpdateBy());
 			if (updateUser != null) {
 				response.put("updatedBy", updateUser.getFirst_name() + " " + updateUser.getLast_name());
