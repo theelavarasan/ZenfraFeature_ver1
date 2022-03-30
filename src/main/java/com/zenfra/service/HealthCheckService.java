5a76369f09e0b1a3c1bda71009d4a5db61ed3780
pravin <pravinkumar.krishnasamy@virtualtechgurus.com>
2022-03-30 19:58:02 +0530

    project health_check display issues

--- src/main/java/com/zenfra/service/HealthCheckService.java
+++ src/main/java/com/zenfra/service/HealthCheckService.java
@@ -327,14 +327,13 @@ public class HealthCheckService {
 		String query = null;
 		try {
 			if (projectId != null && !projectId.isEmpty()) {
-				query = "select * from health_check where site_key='" + siteKey + "' and report_by ='" + projectId
-						+ "' and is_active='true' and report_by not in (select project_id from project) order by health_check_name ASC";
+				query = "select * from health_check where site_key='" + siteKey + "' and report_by ='" + projectId + "' and is_active = true  order by health_check_name ASC";
 
 			} else {
 				query = "select * from health_check where site_key='" + siteKey
-						+ "' and is_active='true' and report_by not in (select project_id from project) order by health_check_name ASC";
+						+ "' and is_active = true and report_by not in (select project_id from project) order by health_check_name ASC";
 				if (!isTenantAdmin) {
-					query = "select * from health_check where is_active = 'true' and ((create_by = '" + userId
+					query = "select * from health_check where is_active = true and ((create_by = '" + userId
 							+ "' and site_key = '" + siteKey + "') or ((site_access_list like '%" + siteKey
 							+ "%' or site_access_list like '%All%') and (user_access_list like '%" + userId
 							+ "%' or user_access_list  like '%All%'))) and report_by not in (select project_id from project) order by health_check_name ASC";
