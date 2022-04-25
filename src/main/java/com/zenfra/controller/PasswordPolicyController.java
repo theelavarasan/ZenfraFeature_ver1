960ca291945db472a595d6d88fea808d5ad64355
pravin <pravinkumar.krishnasamy@virtualtechgurus.com>
2022-04-25 15:57:01 +0530

    changed URL path for password policy updated

--- src/main/java/com/zenfra/controller/PasswordPolicyController.java
+++ src/main/java/com/zenfra/controller/PasswordPolicyController.java
@@ -15,25 +15,11 @@ import io.swagger.annotations.Api;
 
 @CrossOrigin("*")
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/rest")
 @Api(value = "Password Policy Controller", description = "Password Policy CRUD Operations")
 public class PasswordPolicyController {
 	
 	PasswordPolicyService dao = new PasswordPolicyDao();
-
-//	@RequestMapping(value = "/pwd/policy/create", method = RequestMethod.POST)
-//	public ResponseEntity<Response> createPwdPolicy(@RequestAttribute String userId,
-//			@RequestBody PasswordPolicyModel model, @RequestAttribute List<ArrayList<String>>value) {
-//		System.out.println("!!!!!!!!!Password Policy Expire:" + model.getPwdExpire());
-//		System.out.println("!!!!!!!!!Password Policy Lock:" + model.getPwdLock());
-//		System.out.println("!!!!!!!!!Password Policy Length:" + model.getPwdLength());
-//		System.out.println("!!!!!!!!!Password Policy AlphaUpper:	" + model.isAlphaUpper());
-//		System.out.println("!!!!!!!!!Password Policy AlphaLower:	" + model.isAlphaLower());
-//		System.out.println("!!!!!!!!!Password Policy NonAlphaNumeric:" + model.isNonAlphaNumeric());
-//		System.out.println("!!!!!!!!!Password Policy Numbers:	" + model.isNumbers());
-//		System.out.println("!!!!!!!!!Password Policy NonFnIn:" + model.isNonFnIn());
-//		return ResponseEntity.ok(dao.createPwdPolicy(userId, model, value));
-//	}
 
 	@RequestMapping(value = "/pwd/policy/update", method = RequestMethod.PUT)
 	public ResponseEntity<Response> updatePwdPolicy(@RequestParam String userId, @RequestParam String tenantId,
