76b15e6ef018c1c287176e03c46e86096e49f6bc
Ahmed <ahmed.mohammed@virtualtechgurus.com>
2022-05-10 19:11:32 +0530

    smtp debug

--- src/main/java/com/zenfra/service/SmtpConfigService.java
+++ src/main/java/com/zenfra/service/SmtpConfigService.java
@@ -59,39 +59,50 @@ public class SmtpConfigService {
 
 	public ResponseEntity<?> updateSmtpData(SmtpConfigModel smtpConfigModel) {
 		try {
-			SmtpConfigModel existingSmtpData = smtpConfigRepository.findByTenantId(smtpConfigModel.getTenantId()== null ? "" : smtpConfigModel.getTenantId()).orElse(smtpConfigModel);
-			
-			existingSmtpData.setConfigId(smtpConfigModel.getConfigId() == null ? "" : smtpConfigModel.getConfigId());
-			existingSmtpData.setFromAddress(smtpConfigModel.getFromAddress() == null ? "" : smtpConfigModel.getFromAddress());
-			existingSmtpData.setSenderHost(smtpConfigModel.getSenderHost() == null ? "" : smtpConfigModel.getSenderHost());
-			existingSmtpData.setSenderPassword(smtpConfigModel.getSenderPassword() == null ? "" : smtpConfigModel.getSenderPassword());
-			existingSmtpData.setSenderPort(smtpConfigModel.getSenderPort() == null ? "" : smtpConfigModel.getSenderPort());
-			existingSmtpData.setSenderUsername(smtpConfigModel.getSenderUsername() == null ? "" : smtpConfigModel.getSenderUsername());
-			existingSmtpData.setTransportProtocol(smtpConfigModel.getTransportProtocol() == null ? "" : smtpConfigModel.getTransportProtocol());
-			existingSmtpData.setTenantId((smtpConfigModel.getTenantId()== null ? "" : smtpConfigModel.getTenantId()));
-			
+			SmtpConfigModel existingSmtpData = smtpConfigRepository
+					.findByTenantId(smtpConfigModel.getTenantId() == null ? "" : smtpConfigModel.getTenantId())
+					.orElse(smtpConfigModel);
+
+			existingSmtpData
+					.setFromAddress(smtpConfigModel.getFromAddress() == null ? "" : smtpConfigModel.getFromAddress());
+			existingSmtpData
+					.setSenderHost(smtpConfigModel.getSenderHost() == null ? "" : smtpConfigModel.getSenderHost());
+			existingSmtpData.setSenderPassword(
+					smtpConfigModel.getSenderPassword() == null ? "" : smtpConfigModel.getSenderPassword());
+			existingSmtpData
+					.setSenderPort(smtpConfigModel.getSenderPort() == null ? "" : smtpConfigModel.getSenderPort());
+			existingSmtpData.setSenderUsername(
+					smtpConfigModel.getSenderUsername() == null ? "" : smtpConfigModel.getSenderUsername());
+			existingSmtpData.setTransportProtocol(
+					smtpConfigModel.getTransportProtocol() == null ? "" : smtpConfigModel.getTransportProtocol());
+			existingSmtpData.setTenantId((smtpConfigModel.getTenantId() == null ? "" : smtpConfigModel.getTenantId()));
+
 			smtpConfigRepository.save(existingSmtpData);
-			
+
 			responseModel.setResponseMessage("Success");
 			responseModel.setStatusCode(200);
 			responseModel.setResponseCode(HttpStatus.OK);
 			responseModel.setjData(existingSmtpData);
 			return ResponseEntity.ok(responseModel);
-			
-		}catch (Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 			responseModel.setStatusCode(500);
 			responseModel.setResponseCode(HttpStatus.EXPECTATION_FAILED);
 			return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
 		}
-		
-		
-		
+
 	}
 
 	public ResponseEntity<?> deleteSmtpData(String tenantId) {
 		try {
-			smtpConfigRepository.deleteByTenantId(tenantId);
+
+			Optional<SmtpConfigModel> smtpData = smtpConfigRepository.findByTenantId(tenantId);
+
+			if (smtpData.isPresent()) {
+				SmtpConfigModel smtpDataObj = smtpData.get();
+				smtpConfigRepository.delete(smtpDataObj);
+			}
 
 			responseModel.setResponseMessage("Success");
 			responseModel.setStatusCode(200);
