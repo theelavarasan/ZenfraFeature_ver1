8b450c80f05db4cf92b1f436ed4f9fb5b30860ca
Senthil <Senthil@DESKTOP-MFOO58G>
2022-05-17 18:51:58 +0530

    netapp fav view fixed

--- src/main/java/com/zenfra/service/FavouriteApiService_v2.java
+++ src/main/java/com/zenfra/service/FavouriteApiService_v2.java
@@ -192,7 +192,7 @@ public class FavouriteApiService_v2 {
 	}
 
 	private Map<String, Object> filterFavViewByProcessedLogs(Map<String, Object> row, List<String> processedLogs) {
-		
+		System.out.println("------processedLogs-------------- "  +processedLogs);
 		try {
 			if (row.get("filterProperty") != null && !row.get("filterProperty").equals("[]")) {
 				JSONArray filterProp =  (JSONArray) jSONParser.parse(row.get("filterProperty").toString().replace("\\[", "").replace("\\]", ""));
@@ -200,8 +200,13 @@ public class FavouriteApiService_v2 {
 					JSONObject prop = (JSONObject) filterProp.get(i);
 					if(prop.containsKey("selection")) {
 						String values = prop.get("selection").toString().trim();
+						if(values.toLowerCase().contains("mode")) {
+							values = "NETAPP";
+						}
+						System.out.println("------values-------------- "  +values);
+						
 						if(processedLogs.stream().anyMatch(values::equalsIgnoreCase)) {
-							return common.getFavViewCheckNull(row);
+							return common.getFavViewCheckNull(row);							
 						}
 					}
 				}
