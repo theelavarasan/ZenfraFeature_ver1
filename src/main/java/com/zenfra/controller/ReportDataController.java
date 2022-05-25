8d4e33df601813af64359a67f8a12d5fa7c2b219
Senthil <Senthil@DESKTOP-MFOO58G>
2022-05-25 22:58:25 +0530

    fix

--- src/main/java/com/zenfra/controller/ReportDataController.java
+++ src/main/java/com/zenfra/controller/ReportDataController.java
@@ -9,7 +9,6 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.net.URLConnection;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
@@ -26,7 +25,6 @@ import org.apache.commons.io.FileUtils;
 import org.apache.spark.sql.SparkSession;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.roaringbitmap.longlong.Roaring64NavigableMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -38,13 +36,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
-import com.als.bo.Employer;
-import com.als.util.GetUserSession;
 import com.clickhouse.jdbc.ClickHouseDataSource;
 import com.zenfra.dataframe.request.ServerSideGetRowsRequest;
 import com.zenfra.dataframe.response.DataResult;
@@ -603,45 +597,8 @@ public class ReportDataController {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-       
-		
 		 
 	}
-	
-	
-	 @RequestMapping(value = "/dwnload", method = RequestMethod.GET)
-	    public ModelAndView doDownload(@RequestParam("filePath") String filePath, HttpServletRequest request,
-	                                   HttpServletResponse response) throws IOException {
-	        if (GetUserSession.getUserSession() == null) {
-	            return new ModelAndView("redirect:/logoutInternal");
-	        }
-
-	        ModelAndView model = new ModelAndView();
-	        List<Employer> employer_list = userservice.showEmployerGrid();
-	        model.addObject("employer_list", employer_list);
-
-	        File file = new File(filePath);
-
-	        String mimeType = URLConnection.guessContentTypeFromName(file.getName());
-	        if (mimeType == null) {
-	            mimeType = "application/octet-stream";
-	        }
-
-	        response.setContentType(mimeType);
-	        response.setHeader("Content-Disposition", String.format("inline; filename=\"" + file.getName() + "\""));
-	        response.setContentLength((int) file.length());
-
-	        InputStream inputStream = null;
-	        try {
-	            inputStream = new BufferedInputStream(new FileInputStream(file));
-	            FileCopyUtils.copy(inputStream, response.getOutputStream());
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        } finally {
-	            inputStream.close();
-	        }
-	        return new ModelAndView("FileProcessing");
-	    }
 	
 
 }
