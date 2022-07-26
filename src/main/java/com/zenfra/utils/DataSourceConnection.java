a74c3c26ab5b02d4e6beac9c01ee0a468b3b9121
pravin <pravinkumar.krishnasamy@virtualtechgurus.com>
2022-07-26 18:39:01 +0530

    changed security project query

--- src/main/java/com/zenfra/utils/DataSourceConnection.java
+++ src/main/java/com/zenfra/utils/DataSourceConnection.java
@@ -20,7 +20,7 @@ public class DataSourceConnection {
         config.setPassword(ZKModel.getProperty(ZKConstants.POSTGRES_PWD));
         config.addDataSourceProperty("minimumIdle", ZKModel.getProperty(ZKConstants.PG_MIN_IDLE_TIMEOUT));
         config.addDataSourceProperty("maximumPoolSize", ZKModel.getProperty(ZKConstants.PG_MAX_POOL_SIZE));
-        config.setConnectionTimeout(300000);
+        config.setConnectionTimeout(3000000);
         config.setLeakDetectionThreshold(3000000);
         config.setIdleTimeout(600000);
         config.setMaxLifetime(1800000);
