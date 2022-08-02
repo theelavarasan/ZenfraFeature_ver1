c4c0c0f3a7c2cb9319cb823a2b6945a613f4f210
Mohanaprakash <mohanaprakash.jayabalan@virtualtechgurus.com>
2022-08-02 20:57:10 +0530

    debugging

--- src/main/java/com/zenfra/dataframe/service/DataframeService.java
+++ src/main/java/com/zenfra/dataframe/service/DataframeService.java
@@ -3337,65 +3337,7 @@ public JSONObject prepareChartForTanium(JSONObject chartParams) {
 					String breakDownName = (String) breakDown.get("value");
 					JSONArray finalBreakDownValue = new JSONArray();
 					
-					
-					
-					//"select pd.data::json ->> '"+columnName+"' as \"colName\", count(pd.data::json ->> '"+columnName+"') as \"colValue\"   from ( Select pd.source_id,pd.server_name,pd.data,sd.data source_data1,sd1.data source_data2 from privillege_data pd LEFT JOIN source_data sd on sd.site_key = '"+siteKey+"' and sd.primary_key_value = pd.source_id LEFT JOIN source s1 on s1.source_id = sd.source_id LEFT JOIN source_data sd1 on sd1.site_key = '"+siteKey+"' and sd1.primary_key_value = pd.server_name LEFT JOIN source s2 on s2.source_id = sd1.source_id Where pd.site_key = '"+siteKey+"' ) pd group by pd.data::json ->> '"+columnName+"'";
-					Dataset<Row> dataSet = sparkSession.emptyDataFrame();
-
-					String query = "select pd.data::json ->> '"+xaxisColumnName+"'  as \"colName\"";
-
-					if (breakDownName != null && !breakDownName.isEmpty()) {
-						query = query.concat(", pd.data::json ->> '"+breakDownName+"' as `colBreakdown`");
-					}
-					for (int i = 0; i < yaxisNames.size(); i++) {
-						String operater = (String) classNameArray.get(i);
-						if (operater.contains("count")) {
-							query = query.concat(", count(pd.data::json ->> '" + yaxisNames.get(i) + "') as \"colValue" + i+"\"");
-						} else if (operater.contains("sum")) {
-							query = query.concat(", sum((pd.data::json ->> '" + yaxisNames.get(i) + "')::int) as \"colValue" + i+"\"");
-						}
-					}
-
-					query = query.concat(" from ( Select pd.source_id,pd.server_name,pd.data,sd.data source_data1,sd1.data source_data2 from privillege_data pd LEFT JOIN source_data sd on sd.site_key = '"+siteKey+"' and sd.primary_key_value = pd.source_id LEFT JOIN source s1 on s1.source_id = sd.source_id LEFT JOIN source_data sd1 on sd1.site_key = '"+siteKey+"' and sd1.primary_key_value = pd.server_name LEFT JOIN source s2 on s2.source_id = sd1.source_id Where pd.site_key = '"+siteKey+"' ) pd ");
-
-//conditions for filtering
-					if(!filterModel.isEmpty() && filterModel != null) {
-						
-						JSONObject filterModelObject = filterModel;
-						Set<String> filterKeys = new HashSet<>();
-						for (int i = 0; i < filterModelObject.size(); i++) {
-							JSONObject jsonObj = filterModelObject;
-							filterKeys.addAll(jsonObj.keySet());
-						}
-
-						query = query.concat(" where ");
-						for (String key : filterKeys) {
-							JSONObject filterColumnName = (JSONObject) filterModelObject.get(key);
-							query = query.concat("pd.data::json ->> '" + key + "'");
-							
-							for(int i = 0; i < (filterModelObject.size() >= 2 ? filterModelObject.size() / 2 : filterModelObject.size()); i++) {
-								if (filterColumnName.containsKey("type")) {
-									query = query.concat(" ilike ");
-								}
-								if (filterColumnName.containsKey("filter")) {
-									query = query.concat("pd.data::json ->> '" + filterColumnName.get("filter") + "'");
-								}
-										query = query.concat(" and ");
-							}
-							
-						}
-						query = query.substring(0, query.length()-5);
-					}
-					
-//conditions for filtering
-					
-					query = query.concat(" group by pd.data::json ->> '" + xaxisColumnName + "'");
-											
-					if (breakDownName != null && !breakDownName.isEmpty()) {
-						query = query.concat(", pd.data::json ->>'" + breakDownName + "'");
-					}	
-					
-					System.out.println("------table query------ " + query);
+					String query = priviledgeChartQueries(chartConfig, siteKey, chartType);
 					
 					List<Map<String, Object>> resultSet = reportDao.getListOfMapByQuery(query);	
 					JSONObject resultObject = new JSONObject(); 
